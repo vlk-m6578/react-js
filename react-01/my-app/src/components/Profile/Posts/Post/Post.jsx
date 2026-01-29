7fa22067c82a8ae13bbbc219f49bc74036931233
Milana L <lebedeva310@icloud.com>
2026-01-29 23:52:22 +0300

    props

--- react-01/my-app/src/components/Profile/Posts/Post/Post.jsx
+++ react-01/my-app/src/components/Profile/Posts/Post/Post.jsx
@@ -1,12 +1,15 @@
 import c from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={c.post}>
+      <div className={c.post__content}>
       <img src='https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4855.jpg'></img>
       <div className={c.post__desc}>
-        post1
+        {props.message}
       </div>
+      </div>
+      <span>{props.count} likes</span>
     </div>
   );
 }
--- react-01/my-app/src/components/Profile/Posts/Posts.jsx
+++ react-01/my-app/src/components/Profile/Posts/Posts.jsx
@@ -4,9 +4,9 @@ import c from './Posts.module.css';
 const Posts = () => {
   return (
     <div className={c.posts}>
-      <Post />
-      <Post />
-      <Post />
+      <Post message='Hi, how are you?' count='0' />
+      <Post message='This is my first project.' count='63' />
+      <Post message='Happy birthday to me!!!!' count='127' />
     </div>
   );
 }
