70d46d11bc6273bde1698e85a8612fbafdf19ead
Milana L <lebedeva310@icloud.com>
2026-01-31 22:49:09 +0300

    refactoring

--- react-01/my-app/src/components/Dialogs/Dialogs.jsx
+++ react-01/my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,28 +1,34 @@
 import { NavLink } from 'react-router-dom';
 import c from './Dialogs.module.css';
 
+const Dialog = (props) => {
+  return (
+    <div className={`${c.dialogs__item} ${c.dialogs__item__active}`}>
+      <NavLink to={'/messages/' + props.id}>{props.name}</NavLink>
+    </div>
+  );
+}
+
+const Message = (props) => {
+  return(
+    <div className={c.message}>{props.message}</div>
+  );
+}
+
 const Dialogs = () => {
   return (
     <div className={c.dialogs}>
       <div className={c.dialogs__list}>
-        <div className={`${c.dialogs__item} ${c.dialogs__item__active}`}>
-          <NavLink to='/messages/1'>Dzmitry</NavLink>
-        </div>
-        <div className={c.dialogs__item}>
-          <NavLink to='/messages/2'>Kate</NavLink>
-        </div>
-        <div className={c.dialogs__item}>
-          <NavLink to='/messages/3'>Milana</NavLink>
-        </div>
-        <div className={c.dialogs__item}>
-          <NavLink to='/messages/4'>Vova</NavLink>
-        </div>
+        <Dialog name='Dzmitry' id='1' />
+        <Dialog name='Kate' id='2' />
+        <Dialog name='Milana' id='3' />
+        <Dialog name='Vova' id='4' />
       </div>
       <div className={c.messages}>
-        <div className={c.message}>Hello bro!</div>
-        <div className={c.message}>How are you?</div>
-        <div className={c.message}>Wow!</div>
-        <div className={c.message}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, illum!</div>
+        <Message message='Hello bro!' />
+        <Message message='How are you?' />
+        <Message message='Wow!' />
+        <Message message='Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, illum!' />
       </div>
     </div>
   );
--- react-01/my-app/src/components/Profile/Profile.jsx
+++ react-01/my-app/src/components/Profile/Profile.jsx
@@ -1,14 +1,11 @@
 import Posts from './Posts/Posts';
 import c from './Profile.module.css';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
   return (
     <div className={c.profile}>
-      <img src='https://media.istockphoto.com/id/1632524142/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D1%81%D0%B5%D1%80%D1%8B%D0%B9-%D1%81%D0%B8%D0%BD%D0%B8%D0%B9-%D0%B1%D0%B5%D0%BB%D1%8B%D0%B9-%D0%B7%D0%B5%D1%80%D0%BD%D0%B8%D1%81%D1%82%D1%8B%D0%B9-%D0%B3%D1%80%D0%B0%D0%B4%D0%B8%D0%B5%D0%BD%D1%82%D0%BD%D1%8B%D0%B9-%D1%84%D0%BE%D0%BD-%D0%BF%D0%BB%D0%B0%D0%BA%D0%B0%D1%82-%D1%84%D0%BE%D0%BD-%D1%88%D1%83%D0%BC-%D1%82%D0%B5%D0%BA%D1%81%D1%82%D1%83%D1%80%D0%B0-%D0%B7%D0%B0%D0%B3%D0%BE%D0%BB%D0%BE%D0%B2%D0%BE%D0%BA-%D0%B2%D0%B5%D0%B1.jpg?s=612x612&w=0&k=20&c=krFOZaNbEAsJBbeNUNnk_bh_0WHR43fHdNYJCsXiswk='></img>
-      <div className={c.profile__desc}>
-        <img src='https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg'></img>
-        Description
-      </div>
+      <ProfileInfo />
       <Posts />
     </div>
   );
