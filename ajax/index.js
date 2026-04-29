1e7494bbb58280b86199886e9a711688461d6955
Milana L <lebedeva310@icloud.com>
2026-04-29 14:47:05 +0300

    promise/axios

--- ajax/index.js
+++ ajax/index.js
@@ -1,29 +1,41 @@
 const resultBlock = document.querySelector("#result");
 const clickMeButton = document.querySelector("#click-me");
-const pageNumber = document.querySelector("#page-number");
+const textInput = document.querySelector("#input-text");
 
-clickMeButton.addEventListener("click", () => { getApis(pageNumber.value, onApisReceived) });
+clickMeButton.addEventListener("click", () => {
+  // const promise = getApis(pageNumber.value);
+  // promise.then(onApisReceived);
 
-function onApisReceived(data) {
-  data.data.forEach(el => {
-    const a = document.createElement('a');
-    a.href = el.api_link
-    a.innerHTML = 'api ' + el.id;
-    document.querySelector('body').insertAdjacentElement("beforebegin", a);
-  })
+  const promise2 = getDogImage();
+  promise2.then(onRandomDogReceived);
+
+  const promise = getJoke();
+  promise.then(onRandomJokeReceived);
+});
+
+// function onApisReceived(data) {
+//   data.data.forEach(el => {
+//     const a = document.createElement('a');
+//     a.href = el.api_link
+//     a.innerHTML = 'api ' + el.id;
+//     document.querySelector('body').insertAdjacentElement("beforebegin", a);
+//   })
+// }
+
+function onRandomDogReceived(data) {
+  const img = document.createElement('img');
+  img.src = data.message;
+  img.className = 'dog-image';
+  document.querySelector('body').appendChild(img);
 }
 
-// function onRandomDogReceived(data) {
-//   const img = document.createElement('img');
-//   img.src = data.message;
-//   img.className = 'dog-image';
-//   document.querySelector('body').appendChild(img);
-// }
+function onRandomJokeReceived(data) {
+  const h1 = document.createElement('h1');
+  h1.innerHTML = data.setup;
+  const p = document.createElement('p');
+  p.innerHTML = data.punchline;
+  p.title = "type: " + data.type;
+  document.querySelector('body').prepend(h1, p)
+}
 
-// function onRandomJokeReceived(data) {
-//   const h1 = document.createElement('h1');
-//   h1.innerHTML = data.setup;
-//   const p = document.createElement('p');
-//   p.innerHTML = data.punchline + 'type:' + data.type;
-//   document.querySelector('body').prepend(h1, p)
-// }
+
--- ajax/service.js
+++ ajax/service.js
@@ -1,33 +1,18 @@
-function getApis(pageNumber, successCallbacks) {
-  $.ajax(`https://api.artic.edu/api/v1/artworks?page=${pageNumber}&limit=3`, {
-    success: successCallbacks
+// function getApis(pageNumber) {
+//   const promise = $.ajax(`https://api.artic.edu/api/v1/artworks?page=${pageNumber}&limit=3`)
+//   return promise;
+// }
+
+function getDogImage() {
+  const promise = axios.get("https://dog.ceo/api/breeds/image/random");
+  return promise.then(response => {
+    return response.data;
   })
-
-  // $.ajax("https://dog.ceo/api/breeds/image/random", {
-  //   success: successCallbacks[1]
-  // })
-
-  // $.ajax("https://official-joke-api.appspot.com/random_joke", {
-  //   success: successCallbacks[2]
-  // })
-
 }
 
-$.ajax("https://dog.ceo/api/breeds/image/random", {
-  success: function (data) {
-    const img = document.createElement('img');
-    img.src = data.message;
-    img.className = 'dog-image';
-    document.querySelector('body').appendChild(img);
-  }
-})
-
-$.ajax("https://official-joke-api.appspot.com/random_joke", {
-  success: function (data) {
-    const h1 = document.createElement('h1');
-    h1.innerHTML = data.setup;
-    const p = document.createElement('p');
-    p.innerHTML = data.punchline + 'type:' + data.type;
-    document.querySelector('body').prepend(h1, p)
-  }
-})
+function getJoke() {
+  const promise = axios.get("https://official-joke-api.appspot.com/random_joke");
+  return promise.then(response => {
+    return response.data;
+  })
+}
