c04e27b747ba06c303e766d008aa949f177cae77
Milana L <lebedeva310@icloud.com>
2026-09-13 12:23:07 +0300

    start project 02

---
+++ react-02/02/02/src/App.tsx
@@ -0,0 +1,28 @@
+
+function App() {
+
+  const tracks = [
+    { id: 1, title: 'Musicfan soundtrack', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3' },
+    { id: 2, title: 'Musicfan soundtrack instrumental', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' }
+  ]
+
+  return (
+    <div>
+      <h1>Musicfun Player</h1>
+      <ul>
+        {tracks.map(track => {
+          return (
+            <li key={track.id}>
+              <div>
+                {track.title}
+              </div>
+              <audio src={track.url} controls></audio>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ react-02/02/02/src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+const rootEl = document.getElementById('root');
+const reactRoot = createRoot(rootEl!);
+reactRoot.render(
+  <App />
+)
