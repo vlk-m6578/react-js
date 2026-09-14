5a9b6c6f896fd2f93b46b8ae00a6935cc2e37545
Milana L <lebedeva310@icloud.com>
2026-09-14 20:39:13 +0300

    conditional rendering / algorithm

--- react-02/02/02/src/App.tsx
+++ react-02/02/02/src/App.tsx
@@ -1,10 +1,19 @@
+// const tracks = null;
+const tracks = [
+  { id: 1, title: 'Musicfan soundtrack', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3' },
+  { id: 2, title: 'Musicfan soundtrack instrumental', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' }
+]
+let selectedTrackId = 1;
 
 export function App() {
 
-  const tracks = [
-    { id: 1, title: 'Musicfan soundtrack', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3' },
-    { id: 2, title: 'Musicfan soundtrack instrumental', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' }
-  ]
+  if (tracks === null) {
+    return <span>loading...</span>
+  }
+
+  if (tracks.length === 0) {
+    return <span>no tracks</span>
+  }
 
   return (
     <div>
@@ -12,7 +21,7 @@ export function App() {
       <ul>
         {tracks.map(track => {
           return (
-            <li key={track.id}>
+            <li key={track.id} style={{ border: track.id === selectedTrackId ? '1px solid orange' : 'none' }}>
               <div>
                 {track.title}
               </div>
