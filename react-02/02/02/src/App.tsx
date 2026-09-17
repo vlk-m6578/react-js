61992127128cccca92f58f6675cd5bc6371da857
Milana L <lebedeva310@icloud.com>
2026-09-17 23:44:05 +0300

    useState

--- react-02/02/02/src/App.tsx
+++ react-02/02/02/src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
+
 // const tracks = null;
 const tracks = [
   { id: 1, title: 'Musicfan soundtrack', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3' },
   { id: 2, title: 'Musicfan soundtrack instrumental', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' }
 ]
-let selectedTrackId = 1;
 
 export function App() {
+
+  const [selectedTrackId, setSelectedTrackId] = useState(1);
 
   if (tracks === null) {
     return <span>loading...</span>
@@ -18,11 +21,12 @@ export function App() {
   return (
     <div>
       <h1>Musicfun Player</h1>
+      <button onClick={() => { setSelectedTrackId(null) }}>reset selection</button>
       <ul>
         {tracks.map(track => {
           return (
             <li key={track.id} style={{ border: track.id === selectedTrackId ? '1px solid orange' : 'none' }}>
-              <div>
+              <div onClick={() => { setSelectedTrackId(track.id) }}>
                 {track.title}
               </div>
               <audio src={track.url} controls></audio>
